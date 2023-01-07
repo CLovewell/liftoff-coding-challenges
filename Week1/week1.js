27dc93eacf65e16ed5798d87dd25c6923a1b439e
Carnahan <109775043+CLovewell@users.noreply.github.com>
2023-01-07 17:54:04 +0000

    Created file structure.

---
+++ Week1/week1.js
@@ -0,0 +1,7 @@
+function triangleArea (base, height) {
+  return (1/2) * base * height;
+}
+
+module.exports = {
+  triangleArea: triangleArea
+};
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-function triangleArea (base, height) {
-  return (1/2) * base * height;
-}
+const week1 = require("./Week1/week1.js");
+
+console.log(week1.triangleArea(4, 8));
